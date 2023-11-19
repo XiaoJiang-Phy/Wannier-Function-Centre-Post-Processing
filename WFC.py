644e70bc90aab624ef43e8e45b596b6677785de5
Xiao Jiang <65940810+jiangx1994@users.noreply.github.com>
2023-11-19 18:06:12 +0800

    Update WFC.py

--- WFC.py
+++ WFC.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# 读取和解析第一个文件的函数
+# A function that reads and parses the first file
 def parse_oxygen_coordinates(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -10,7 +10,7 @@ def parse_oxygen_coordinates(file_path):
     df[['X', 'Y', 'Z']] = df[['X', 'Y', 'Z']].astype(float)
     return df
 
-# 读取和解析第二个文件的函数，特别注意处理逗号分隔的坐标数据
+# A function that reads and parses the second file, paying special attention to working with comma-separated coordinate data
 def parse_wannier_centers(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -18,7 +18,7 @@ def parse_wannier_centers(file_path):
     for line in lines[2:-1]:
         parts = line.strip().split()
         atom_number, projection, no = parts[:3]
-        xyz = ' '.join(parts[3:6]).split(', ')  # 处理逗号分隔的坐标
+        xyz = ' '.join(parts[3:6]).split(', ')  # Handles comma-separated coordinates
         spread = parts[6]
         if len(xyz) == 3:
             data.append([atom_number, projection, no] + xyz + [spread])
@@ -26,20 +26,20 @@ def parse_wannier_centers(file_path):
     df[['NO.', 'X', 'Y', 'Z', 'Spread']] = df[['NO.', 'X', 'Y', 'Z', 'Spread']].astype(float)
     return df
 
-# 解析两个文件
-oxygen_df = parse_oxygen_coordinates('O_site.dat')  # 替换为实际文件路径
-wannier_df = parse_wannier_centers('centre_site.dat')  # 替换为实际文件路径
+# parses two files
+oxygen_df = parse_oxygen_coordinates('O_site.dat')  # Replace with the actual file path
+wannier_df = parse_wannier_centers('centre_site.dat')  # Replace with the actual file path
 
-# 合并数据框，基于原子编号
+# Merge data boxes, based on atomic numbers
 merged_df = pd.merge(wannier_df, oxygen_df, on='Atom Number', suffixes=('_Wannier', '_Atom'))
 
-# 计算距离
+# Calculate the distance between the atom and the Wannnier function centre
 merged_df['Distance'] = np.sqrt((merged_df['X_Wannier'] - merged_df['X_Atom'])**2 + 
                                 (merged_df['Y_Wannier'] - merged_df['Y_Atom'])**2 + 
                                 (merged_df['Z_Wannier'] - merged_df['Z_Atom'])**2)
 
-# 输出到 CSV 文件
-output_csv_path = 'atom_wannier_distances.csv'  # 替换为期望的输出文件路径
+# Output CSV file
+output_csv_path = 'atom_wannier_distances.csv'  # Replace with the desired output file path
 merged_df.to_csv(output_csv_path, index=False)
 
-print(f"数据已成功保存到 {output_csv_path}")
+print(f"The data was saved to {output_csv_path}")
