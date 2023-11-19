a0b63d51a53787f6ba4572f86fa607d15a8b5f3a
Xiao Jiang <65940810+jiangx1994@users.noreply.github.com>
2023-11-19 16:48:15 +0800

    Add files via upload

---
+++ find_atom_site.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+key1='O'
+file_name='WO3.wout'
+atoms='1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'
+rm $key1"_site.dat"
+echo "# Cartesian Coordinate (Ang)" >> $key1"_site.dat"
+echo "# atoms x y z" >> $key1"_site.dat"
+for i in ${atoms}
+ do
+    grep "$key1    $i" "$file_name" | awk '{print $3, $8, $9, $10}' >> $key1"_site.dat"
+    grep "$key1   $i " "$file_name" | awk '{print $3, $8, $9, $10}' >> $key1"_site.dat"
+done
---
+++ find_centre_site.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#Set the context keyword
+keyword_before='Final State'
+keyword_after='Spreads (Ang^2)'
+#
+#
+input_file='WO3.wout'
+#
+rm centre.dat
+awk "/$keyword_before/,/$keyword_after/" "$input_file" >> centre.dat
+rm centre_site.dat
+echo "# Cartesian Coordinate (Ang)" >> centre_site.dat
+echo "# atoms projections NO. x y z spread" >> centre_site.dat
+atoms='1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17'
+proj='pz px py'
+j=0
+for i in ${atoms}
+ do
+   for p in ${proj}
+    do
+       ((j++))
+       grep "$j  (" "centre.dat" | awk -v c1=$i -v c2=$p '{print c1, c2, $5, $7, $8, $9, $11}' >> centre_site.dat
+    done
+ done
+grep "Sum of centres and spreads" "centre.dat"| awk '{print "#", $0}' >> centre_site.dat
+
